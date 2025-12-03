3383e43eb658dbbd557a29090767112fe0d3e439
Praveen Uppar <praveenuppar718@gmail.com>
2025-12-03 22:29:34 +0530

    files

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,21 +2,11 @@ import brainwave from "./brainwave.svg";
 import check from "./check.svg";
 import brainwaveSymbol from "./logo.svg";
 import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
-import play from "./play.svg";
 import gradient from "./gradient.png";
 import grid from "./grid.png";
 import check2 from "./check-02.svg";
 import loading1 from "./loading-01.svg";
-import yourlogo from "./yourlogo.svg";
-import homeSmile from "./home-smile.svg";
-import file02 from "./file-02.svg";
-import searchMd from "./search-md.svg";
-import plusSquare from "./plus-square.svg";
-import recording03 from "./recording-03.svg";
-import recording01 from "./recording-01.svg";
-import disc02 from "./disc-02.svg";
-import chromecast from "./chrome-cast.svg";
-import sliders04 from "./sliders-04.svg";
+
 import loading from "./loading.png";
 
 import curve from "./hero/curve.png";
@@ -45,21 +35,10 @@ export {
   check,
   brainwaveSymbol,
   brainwaveWhiteSymbol,
-  play,
   gradient,
   grid,
   check2,
   loading1,
-  yourlogo,
-  homeSmile,
-  file02,
-  searchMd,
-  plusSquare,
-  recording03,
-  recording01,
-  disc02,
-  chromecast,
-  sliders04,
   loading,
   curve,
   robot,
